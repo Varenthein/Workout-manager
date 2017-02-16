c26b6d0203d91b7ccbc7dc3c5d04da2cc71eb4ba
Paweł Zagrobelny <raventhein@gmail.com>
2017-02-16 10:56:24 +0100

    Minor changes

--- js/script.js
+++ js/script.js
@@ -39,15 +39,18 @@ Element.prototype.fadeOut = function(callbackF) {
 
 Number.prototype.formatTime = () => {
 
-    let sec_num = parseInt(this, 10);
-    let hours   = Math.floor(sec_num / 3600);
-    let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    let seconds = sec_num - (hours * 3600) - (minutes * 60);
+    if(isNaN(parseInt(this))) return "00:00:00";
+    else {
+      let sec_num = parseInt(this, 10);
+      let hours   = Math.floor(sec_num / 3600);
+      let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+      let seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    return hours+':'+minutes+':'+seconds;
+      if (hours   < 10) {hours   = "0"+hours;}
+      if (minutes < 10) {minutes = "0"+minutes;}
+      if (seconds < 10) {seconds = "0"+seconds;}
+      return hours+':'+minutes+':'+seconds;
+    }
 }
 
 //play workout function
